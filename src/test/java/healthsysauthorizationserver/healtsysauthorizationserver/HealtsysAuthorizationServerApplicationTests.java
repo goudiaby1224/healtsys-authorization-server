8b48b453cc8092081e402426687aaaa9bdb74629
goudiaby1224 <goudiaby1224@gmail.com>
2023-10-29 12:10:20 -0400

    Configure Resource server

--- src/test/java/healthsysauthorizationserver/healtsysauthorizationserver/HealtsysAuthorizationServerApplicationTests.java
+++ src/test/java/healthsysauthorizationserver/healtsysauthorizationserver/HealtsysAuthorizationServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HealtsysAuthorizationServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
